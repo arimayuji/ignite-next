5f1bf858e84541d75b807168b4defeece249ceb8
Yuji Arima <yujiarima@hotmail.com>
2024-04-27 10:28:57 -0300

    big fuck commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		formats: ['image/avif', 'image/webp'],
+		remotePatterns: [
+		  {
+			protocol: 'https',
+			hostname: 'files.stripe.com',
+			port: '',
+			pathname: '/links/**'
+		  },
+		],
+	  },
+	
 };
 
 export default nextConfig;
